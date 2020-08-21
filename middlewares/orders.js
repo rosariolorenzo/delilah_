611961fd24c57cfd0723ffa122027a22ca2465a4
Rosario Lorenzo <rochi.lorenzo@gmail.com>
2020-08-21 18:42:33 -0300

    middlewares order ready

--- middlewares/orders.js
+++ middlewares/orders.js
@@ -1,1 +1,38 @@
  
+function validatePostOrder(  req, res, next ) {
+    const { productsId, usernameId, payment_methodId, delivery_address } = req.body;
+    if( !productsId || !usernameId || !payment_methodId || !delivery_address || !Array.isArray(req.body.productsId) || !req.body.productsId.length  ) {
+        res.statusCode = 400;
+        res.json("invalid properties");
+    } else {
+        next();
+    }
+}
+
+function validateUpdateOrder(  req, res, next ) {
+    const { status_id } = req.body;
+
+    if( !status_id ) {
+        res.statusCode = 400;
+        res.json("invalid properties");
+    } else {
+        next();
+    }
+}
+
+function validateStatusOrderProps(  req, res, next ) {
+    const { description } = req.body;
+
+    if( !description ) {
+        res.statusCode = 400;
+        res.json("invalid properties");
+    } else {
+        next();
+    }
+}
+
+
+
+module.exports.validatePostOrder = validatePostOrder;
+module.exports.validateStatusOrderProps = validateStatusOrderProps;
+module.exports.validateUpdateOrder = validateUpdateOrder;
